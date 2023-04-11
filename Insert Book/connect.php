c1127df5f53bf96c3a0379dde818c5648ec12108
dsuborno <73629539+dsuborno@users.noreply.github.com>
2023-04-11 22:50:06 +0530

    #commit

---
+++ Insert Book/connect.php
@@ -0,0 +1,25 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
+    $conn = mysqli_connect('localhost','root','','college') or die("Connection failed:".mysqli_connect_error());
+
+    if(isset($_POST['bname']) && isset($_POST['author']) && isset($_POST['stock'])){
+
+        $name = $_POST['bname'];
+        $author = $_POST['author'];
+        $stock = $_POST['stock'];
+
+        $stmt = mysqli_prepare($conn, "INSERT INTO `books` (book_name,author,stock) VALUES (?, ?, ?)");
+        mysqli_stmt_bind_param($stmt, 'ssi', $name, $author, $stock);
+        $query = mysqli_stmt_execute($stmt);
+
+        if($query){
+            echo 'Entry Successful';
+        }
+        else{
+            echo 'Error Occurred';
+        }
+        mysqli_stmt_close($stmt);
+    }
+    mysqli_close($conn);
+}
+?>
