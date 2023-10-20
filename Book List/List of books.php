8d65f22ac679703e8fbdcea67017ec9af6ac0815
Suborno22 <dsuborno0@gmail.com>
2023-10-20 17:27:43 +0530

    making changes

--- Book List/List of books.php
+++ Book List/List of books.php
@@ -1,7 +1,7 @@
 <?php
 
 // Create connection
-$conn = mysqli_connect("localhost", "root","","college");
+$conn = mysqli_connect("localhost","root","password","college");
 
 // Check connection
 if (!$conn) {
@@ -11,16 +11,13 @@ echo "Connected successfully"."<br>";
 echo "-----------------------------"."<br>";
 
 // Step 2: Create a SQL query to select the data
-$sql = "SELECT * FROM books";
+$sql = "SELECT `book_name` FROM books";
 $result = mysqli_query($conn, $sql);
 
 // Step 3: Loop through the results of the query and display the data
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-        echo "ID: " . $row["id"] . "<br>";
-        echo "Name of the book " . $row["book_name"] . "<br>";
-        echo "Author: " . $row["author"] . "<br>";
-        echo "Stock: " . $row["stock"]."<br><br>" ;
+        echo "Name of the book " . $row["book_name"] . "<br>". "<br>";
     }
 } else {
     echo  "<br>"."Its empty in here";
--- Queries.sql
+++ Queries.sql
@@ -1,25 +1,64 @@
+DROP DATABASE IF EXISTS college;
+
 CREATE DATABASE IF NOT EXISTS college;
 
 USE college;
 
+CREATE TABLE IF NOT EXISTS authors (
+  author_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  author_name VARCHAR(255)
+);
+
 CREATE TABLE IF NOT EXISTS books (
   id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  book_name VARCHAR(100),
-  stock INT(20)
+  book_name VARCHAR(255),
+  publishing_date DATE,
+  author_id INT(11),
+  FOREIGN KEY (author_id) REFERENCES authors (author_id)
 );
 
-CREATE TABLE IF NOT EXISTS history (
+CREATE TABLE IF NOT EXISTS borrowings (
   id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   user_name VARCHAR(100),
-  book_name VARCHAR(100),
-  author_name VARCHAR(100)
+  book_id INT(11),
+  borrow_date DATE
 );
 
-CREATE TABLE IF NOT EXISTS students_database (
+CREATE TABLE IF NOT EXISTS returns (
   id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(250),
-  email VARCHAR(250),
-  phone VARCHAR(100),
+  user_name VARCHAR(100),
+  book_id INT(11),
+  return_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS book_details(
+  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  Stock INT(5) 
+);
+
+CREATE TABLE IF NOT EXISTS courses (
+  course_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  course_name VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS enrollments (
+  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  student_id INT(11),
+  course_id INT(11),
   semester VARCHAR(100),
   department VARCHAR(100)
 );
+
+CREATE TABLE IF NOT EXISTS students (
+  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(250),
+  email VARCHAR(250),
+  phone VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS admins(
+  id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  full_name VARCHAR(255)
+);
+
+show tables;
