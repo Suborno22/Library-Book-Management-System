740f0acffde3b634d10ce67a7202c2ccab8524c2
Suborno22 <dsuborno0@gmail.com>
2023-10-27 11:45:30 +0530

    pushed

---
+++ Login/login.php
@@ -0,0 +1,75 @@
+<?php
+session_start(); // Start the session
+
+if (isset($_SESSION['isLoggedIn'])) {
+    if ($_SESSION['isAdmin'] === true) {
+        header('Location: admin_dashboard.php');
+    } else {
+        header('Location: user_dashboard.php');
+    }
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Connect to the database
+    require(__DIR__ . '/../dbconfig/connect.php');
+
+    // Prepare a query to retrieve user information based on the provided username
+    $query = "SELECT * FROM admins WHERE full_name = ? AND password = ?";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, 'ss', $username, $password);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    if (mysqli_num_rows($result) == 1) {
+        $user = mysqli_fetch_assoc($result);
+
+        // Check if the user is an admin
+        if ($user['status'] === 'admin') {
+            $_SESSION['isAdmin'] = true;
+        } else {
+            $_SESSION['isAdmin'] = false;
+        }
+
+        $_SESSION['isLoggedIn'] = true;
+
+        if ($_SESSION['isAdmin'] === true) {
+            header('Location: admin_dashboard.php');
+        } else {
+            header('Location: user_dashboard.php');
+        }
+        exit();
+    } else {
+        $error = "Invalid username or password";
+    }
+
+    mysqli_close($conn);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login Page</h1>
+    
+    <?php if (isset($error)) { ?>
+        <p style="color: red;"><?php echo $error; ?></p>
+    <?php } ?>
+
+    <form method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" required>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" required>
+        <br>
+        <input type="submit" value="Login" name="submit">
+    </form>
+</body>
+</html>
